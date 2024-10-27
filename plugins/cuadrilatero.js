9064bda70d021a4070d98ee4ad12bd39deb1a3c6
Perez1503 <franklinsilvestreperezmendez@gmail.com>
2024-10-27 16:11:38 -0600

    perez

---
+++ plugins/cuadrilatero.js
@@ -0,0 +1,40 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.cuadrilatero);
+};
+handler.command = /^(CUADRILATERO|cuadrilatero)$/i;
+export default handler;
+
+global.cuadrilatero = `
+CUADRILATERO✨
+
+𝐇𝐎𝐑𝐀𝐑𝐈𝐎
+🇲🇽 𝐌𝐄𝐗 : 00:00PM
+🇨🇴 𝐂𝐎𝐋 : 00:00PM
+𝐂𝐎𝐋𝐎𝐑 𝐃𝐄 𝐑𝐎𝐏𝐀: 
+¬ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒 𝐏𝐑𝐄𝐒𝐄𝐍𝐓𝐄𝐒
+
+      𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1
+
+✨ ┇ 
+🥷🏻 ┇ 
+🥷🏻 ┇ 
+🥷🏻 ┇ 
+      
+     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 2
+
+✨ ┇ 
+🥷🏻 ┇  
+🥷🏻 ┇ 
+🥷🏻 ┇ 
+
+     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 3
+
+✨ ┇
+🥷🏻 ┇ 
+🥷🏻 ┇
+🥷🏻 ┇
+
+ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+🥷🏻 ┇
+🥷🏻 ┇
+`;
